b4c76d32b553360aed6ffb68bac0ee6737a58a52
Daniel Ruffing <danielruffing@wustl.edu>
2020-04-03 12:34:29 -0400

    added tab view manager code to MainActivity() but didn't change gradle files - Daniel Ruffing

---
+++ app/src/main/java/com/example/beerdiary/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.beerdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragmentAdapter = MyPagerAdapter(supportFragmentManager)
+        viewpager_main.adapter = fragmentAdapter
+
+        tabs_main.setupWithViewPager(viewpager_main)
+    }
+}
+
+class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount() : Int {
+        return 3
+    }
+
+    override fun getItem(position: Int) : Fragment {
+        return when (position) {
+            0 -> { CollectionFragment() }
+            1 -> { ExploreFragment() }
+            else -> AddItemFragment()
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            0 -> "Collection"
+            1 -> "Explore"
+            else -> "Add Item"
+        }
+    }
+
+}
