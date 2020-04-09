f323e165d6df672853913ffc8a62513910d51f12
Daniel Ruffing <danielruffing@wustl.edu>
2020-04-09 14:45:40 -0400

    added collection fragment layout- Dan

--- app/src/main/java/com/example/beerdiary/fragments/CollectionFragment.kt
+++ app/src/main/java/com/example/beerdiary/fragments/CollectionFragment.kt
@@ -6,9 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.beerdiary.R
+import com.example.beerdiary.data.Beer
 
 class CollectionFragment: Fragment(){
-    //private var beerList: ArrayList<Beer> = ArrayList()
+    private var beerList: ArrayList<Beer> = ArrayList()
     //private lateinit var collectionViewModel: CollectionViewModel
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_collection, container, false)
